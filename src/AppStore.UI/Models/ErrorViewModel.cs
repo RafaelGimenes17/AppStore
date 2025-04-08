a35666a8fff2d4ebee4dfc7591edf9553fccf27e
Rafael Gimenes <gimenes_rafael_f1@cat.com>
2025-04-08 12:40:53 -0300

    Adicionar arquivos de projeto.

---
+++ src/AppStore.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AppStore.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
